6789bf1835a4d9aad88a07cf834e2d0df29d6662
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-06-23 20:14:06 +0200

    Summary of errors instead of fail.

--- src/main/java/stupaq/vhdl2lv/Compiler.java
+++ src/main/java/stupaq/vhdl2lv/Compiler.java
@@ -1,30 +1,26 @@
 package stupaq.vhdl2lv;
 
-import stupaq.parser.ErrorFailure;
-import stupaq.vhdl93.ParseException;
+import java.io.FileInputStream;
+
+import stupaq.parser.ErrorSummary;
 import stupaq.vhdl93.VHDL93Parser;
 
 public class Compiler {
   public static void main(String args[]) throws Exception {
     if (args.length == 1) {
-      java.io.FileInputStream file = new java.io.FileInputStream(args[0]);
+      FileInputStream file = new FileInputStream(args[0]);
       VHDL93Parser parser = new VHDL93Parser(file);
-      parser.errorHandler = new ErrorFailure();
+      parser.errorHandler = new ErrorSummary();
       try {
         System.out.println("reading from file: " + args[0]);
         parser.design_file();
-        parser.errorHandler.summary();
-      } catch (ParseException p) {
-        System.out.println("syntax error: ");
-        p.printStackTrace();
-        throw p;
+        System.err.println(parser.errorHandler.summary());
       } catch (Exception e) {
-        System.out.println("error: ");
         e.printStackTrace();
         throw e;
       }
     } else {
-      System.out.println("usage: Vhdl filename");
+      System.out.println("usage: filename");
     }
   }
 }
