46459aca1a660c647b6d5f3ef209e693f9b95edb
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-06-26 00:20:37 +0200

    Added instrumentation for error_skipto.

---
+++ src/main/java/stupaq/vhdl93/ast/error_skipto.java
@@ -0,0 +1,35 @@
+package stupaq.vhdl93.ast;
+
+import stupaq.vhdl93.visitor.GJNoArguVisitor;
+import stupaq.vhdl93.visitor.GJVisitor;
+import stupaq.vhdl93.visitor.GJVoidVisitor;
+import stupaq.vhdl93.visitor.Visitor;
+
+public class error_skipto extends SimpleNode implements Node {
+  @Override
+  public void accept(Visitor v) {
+  }
+
+  @Override
+  public <R, A> R accept(GJVisitor<R, A> v, A argu) {
+    return null;
+  }
+
+  @Override
+  public <R> R accept(GJNoArguVisitor<R> v) {
+    return null;
+  }
+
+  @Override
+  public <A> void accept(GJVoidVisitor<A> v, A argu) {
+  }
+
+  @Override
+  public void setParent(Node n) {
+  }
+
+  @Override
+  public Node getParent() {
+    return null;
+  }
+}
