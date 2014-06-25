06fda9b0d198c773fbd947f0617f3f5ab23185bf
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-06-25 13:05:56 +0200

    Removed context checking and all JTB non-compatible code. Will need to recreate it.

--- src/main/java/stupaq/lv2vhdl/LV2VHDL.java
+++ src/main/java/stupaq/lv2vhdl/LV2VHDL.java
@@ -5,6 +5,10 @@ import java.io.PrintWriter;
 
 import stupaq.parser.ErrorSummary;
 import stupaq.vhdl93.VHDL93Parser;
+import stupaq.vhdl93.ast.Node;
+import stupaq.vhdl93.ast.SimpleNode;
+import stupaq.vhdl93.visitor.TreeDumper;
+import stupaq.vhdl93.visitor.TreeFormatter;
 
 public class LV2VHDL {
   public static void main(String args[]) throws Exception {
@@ -12,14 +16,21 @@ public class LV2VHDL {
     if (args.length == 1) {
       FileInputStream file = new FileInputStream(args[0]);
       VHDL93Parser parser = new VHDL93Parser(file);
-      parser.setErrorHandler(new ErrorSummary());
       try {
-        System.out.println("reading from file: " + args[0]);
-        parser.design_file();
-        System.err.println(parser.getErrorHandler().summary());
-        VHDLEmitterVisitor emitter = new VHDLEmitterVisitor(new PrintWriter(System.out));
-        parser.rootNode().jjtAccept(emitter, null);
-        emitter.flush();
+        SimpleNode root = parser.design_file();
+        System.out.println();
+        System.out.println("pre-formatting");
+        System.out.println("==============");
+        TreeDumper dumper = new TreeDumper(System.out);
+        dumper.startAtNextToken();
+        root.accept(dumper);
+        System.out.println();
+        System.out.println();
+        System.out.println("post-formatting");
+        System.out.println("===============");
+        root.accept(new TreeFormatter(4, 120));
+        dumper.resetPosition();
+        root.accept(dumper);
       } catch (Exception e) {
         e.printStackTrace();
         throw e;
---
+++ src/main/java/stupaq/vhdl93/ast/SimpleNode.java
@@ -0,0 +1,6 @@
+package stupaq.vhdl93.ast;
+
+import stupaq.vhdl93.ast.Node;
+
+public abstract class SimpleNode implements Node {
+}
