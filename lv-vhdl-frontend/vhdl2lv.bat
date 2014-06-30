44ab86f5aab7d402d7d021795fcf4e5001c670b1
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-06-30 14:13:10 +0200

    Added lv-scripting dependency as a submodule.

--- lv-vhdl-frontend/vhdl2lv.bat
+++ lv-vhdl-frontend/vhdl2lv.bat
@@ -1,7 +1,6 @@
-@SET scripting_dir=..\..\lv-scripting
 @SET target_vhdl=..\..\testing\main.vhd
 @SET project_dir=..\..\testing
 
-"C:\Program Files\Java\jdk1.7.0_60\bin\java.exe" -cp "target\classes;..\lv-scripting-java\target\classes;target\dependency\*" -Dscripting.tools.path="%scripting_dir%" vhdl2lv "%target_vhdl%" "%project_dir%"
+"C:\Program Files\Java\jdk1.7.0_60\bin\java.exe" -cp "target\classes;..\lv-scripting-java\target\classes;target\dependency\*" -Dscripting.tools.path="..\lv-scripting" vhdl2lv "%target_vhdl%" "%project_dir%"
 
 @PAUSE
