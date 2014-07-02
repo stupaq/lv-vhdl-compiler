fd4f166a9fa75fccf68bc601ef54e728ad769d38
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-02 10:59:10 +0200

    Removed lv-scripting submodule.

--- lv-vhdl-frontend/vhdl2lv.bat
+++ lv-vhdl-frontend/vhdl2lv.bat
@@ -1,6 +1,6 @@
 @SET target_vhdl=..\..\testing\main.vhd
 @SET project_dir=..\..\testing
 
-"C:\Program Files\Java\jdk1.7.0_60\bin\java.exe" -cp "target\classes;..\lv-scripting-java\target\classes;target\dependency\*" -Dscripting.tools.path="..\lv-scripting" vhdl2lv "%target_vhdl%" "%project_dir%"
+"C:\Program Files\Java\jdk1.7.0_60\bin\java.exe" -cp "target\classes;..\lv-scripting-java\target\classes;target\dependency\*" -Dscripting.tools.path="..\lv-scripting-java\lv-scripting" vhdl2lv "%target_vhdl%" "%project_dir%"
 
 @PAUSE
