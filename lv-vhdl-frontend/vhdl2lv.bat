2415ef3db8affea8a103a2f78839d226b8208efa
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-07 15:40:39 +0200

    Tracing StdCallVI calls to file.

--- lv-vhdl-frontend/vhdl2lv.bat
+++ lv-vhdl-frontend/vhdl2lv.bat
@@ -1,6 +1,6 @@
 @SET target_vhdl=..\examples\vhdl2lv\all.vhd
 @SET project_dir=..\examples\vhdl2lv
 
-%JAVA_HOME%\bin\java.exe -cp target\classes;..\lv-scripting-java\target\classes;target\dependency\* -Dscripting.tools.path=..\lv-scripting-java\lv-scripting vhdl2lv "%target_vhdl%" "%project_dir%"
+%JAVA_HOME%\bin\java.exe -cp config;target\classes;..\lv-scripting-java\target\classes;target\dependency\* -Dscripting.tools.path=..\lv-scripting-java\lv-scripting vhdl2lv "%target_vhdl%" "%project_dir%"
 
 @PAUSE
