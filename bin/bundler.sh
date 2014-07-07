72c8d318f582504f6aad1e17d8f0a1a3c54d6025
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-07 17:08:31 +0200

    Fixed bundler to include config/.

--- bin/bundler.sh
+++ bin/bundler.sh
@@ -7,10 +7,12 @@ cd "${basedir}"
 revision="`git rev-parse --short HEAD`"
 
 zip -FSr "bundle-${revision}.zip" \
+  lv-scripting-java/config \
   lv-scripting-java/lv-scripting \
   lv-scripting-java/target/dependency \
   lv-scripting-java/target/classes \
   lv-scripting-java/*.bat \
+  lv-vhdl-frontend/config \
   lv-vhdl-frontend/target/dependency \
   lv-vhdl-frontend/target/classes \
   lv-vhdl-frontend/*.bat \
