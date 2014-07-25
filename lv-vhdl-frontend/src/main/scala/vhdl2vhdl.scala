6cef7e9e8eb66a9a6994caec8a531464a0dde630
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-25 23:07:09 +0200

    Successfully converted some code to Scala ;).

---
+++ lv-vhdl-frontend/src/main/scala/vhdl2vhdl.scala
@@ -0,0 +1,30 @@
+import java.io.FileInputStream
+
+import stupaq.translation.ExceptionPrinter
+import stupaq.translation.semantic.FlattenNestedListsVisitor
+import stupaq.vhdl93.VHDL93Parser
+import stupaq.vhdl93.formatting.VHDLTreeFormatter
+import stupaq.vhdl93.visitor.TreeDumper
+
+object vhdl2vhdl {
+
+  def main(args: Array[String]) {
+    try {
+      if (args.length == 1) {
+        val file = new FileInputStream(args(0))
+        val parser = new VHDL93Parser(file)
+        val root = parser.design_file()
+        root.accept(new FlattenNestedListsVisitor())
+        root.accept(new TreeDumper(System.err))
+        System.err.println()
+        root.accept(new VHDLTreeFormatter())
+        root.accept(new TreeDumper(System.out))
+        println()
+      } else {
+        System.err.println("usage: filename")
+      }
+    } catch {
+      case e: Exception => ExceptionPrinter.print(e, System.err)
+    }
+  }
+}
