6b4a11a802ecbbf211f043ae605df8cbf9ee366d
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-26 15:07:40 +0200

    Scalified drivers.

---
+++ lv-vhdl-frontend/src/main/scala/stupaq/translation/lv2vhdl/TranslationDriver.scala
@@ -0,0 +1,29 @@
+package stupaq.translation.lv2vhdl
+
+import java.nio.file.Paths
+
+import stupaq.labview.VIPath
+import stupaq.translation.ExceptionPrinter
+import stupaq.translation.project.VHDLProject
+
+import scala.collection.JavaConversions._
+
+object TranslationDriver {
+
+  def main(args: Array[String]) {
+    try {
+      if (args.length >= 2) {
+        val roots = args.toStream dropRight 1 map (new VIPath(_))
+        val project = new VHDLProject(Paths get args.last, roots)
+        for (path <- project iterator()) {
+          new VIInstance(project, path) emitAsVHDL()
+        }
+      } else {
+        println("usage: <filename1> <filename2> ... <destination>")
+      }
+    } catch {
+      case e: Exception => ExceptionPrinter print(e, System.err)
+    }
+  }
+}
+
---
+++ lv-vhdl-frontend/src/main/scala/stupaq/translation/vhdl2lv/TranslationDriver.scala
@@ -0,0 +1,34 @@
+package stupaq.translation.vhdl2lv
+
+import java.io.FileInputStream
+import java.nio.file.Paths
+
+import stupaq.translation.ExceptionPrinter
+import stupaq.translation.project.LVProject
+import stupaq.vhdl93.VHDL93Parser
+import stupaq.vhdl93.ast.{NodeList, design_file}
+
+import scala.collection.JavaConversions._
+
+object TranslationDriver {
+
+  def main(args: Array[String]) {
+    try {
+      if (args.length >= 2) {
+        val units = args.toStream dropRight 1 map (new FileInputStream(_)) flatMap
+                    (new VHDL93Parser(_).design_file().nodeList.nodes.toIterable)
+        val list = new NodeList()
+        for (unit <- units) {
+          list addNode unit
+        }
+        val root = new design_file(list)
+        val project = new LVProject(Paths get args.last)
+        root accept new DesignFileEmitter(project)
+      } else {
+        println("usage: <filename1> <filename2> ... <destination>")
+      }
+    } catch {
+      case e: Exception => ExceptionPrinter print(e, System.err)
+    }
+  }
+}
