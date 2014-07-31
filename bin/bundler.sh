e38350e0b3254247b99f05251fe39cc136e52a87
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-31 18:17:42 +0200

    Fixed bundler.

--- bin/bundler.sh
+++ bin/bundler.sh
@@ -7,18 +7,15 @@ cd "${basedir}"
 revision="`git rev-parse --short HEAD`"
 
 zip -FSr "bundle-${revision}.zip" \
-  lv-scripting-java/config \
   lv-scripting-java/lv-scripting \
+  lv-scripting-java/config \
   lv-scripting-java/target/dependency \
   lv-scripting-java/target/classes \
-  lv-scripting-java/*.bat \
   lv-vhdl-frontend/config \
   lv-vhdl-frontend/target/dependency \
   lv-vhdl-frontend/target/classes \
-  lv-vhdl-frontend/*.bat \
-  examples/vhdl2lv/*.vhd \
-  -x examples/vhdl2lv/*.vi \
-  examples/demo/ \
-  -x examples/demo/*.vi \
+  examples/vhdl \
+  examples/*.bat \
+  *.bat \
   -x *.git* \
 
